a79e776eb45120f1407525ba0939ef753b3d1096
natalapovarova <n.povarova3101@yandex.ru>
2020-06-19 13:48:28 +0300

    Генератор текстовых отчетов

--- src/WriteToTxt.java
+++ src/WriteToTxt.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class WriteToTxt {
     private final String path;
@@ -10,8 +12,8 @@ public class WriteToTxt {
     //записываем таблицу в txt файл
 
     public void WriteLastLine(String report) throws IOException {
-        File file = new File(this.path);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        //File file = new File(this.path);
+        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(this.path), Charset.forName("UTF-16").newEncoder());
         try {
             writer.write(report);
         } catch (Exception e) {
